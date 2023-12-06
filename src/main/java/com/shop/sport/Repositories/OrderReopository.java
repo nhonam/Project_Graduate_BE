ae4bfe9bd650a4e0f49ef6e9b17d76a519ecd14f
Dinh Nho Nam <78796281+nhonam@users.noreply.github.com>
2023-12-06 23:27:52 +0700

    pusg

--- src/main/java/com/shop/sport/Repositories/OrderReopository.java
+++ src/main/java/com/shop/sport/Repositories/OrderReopository.java
@@ -12,10 +12,11 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+
 @EnableJpaRepositories
 public interface OrderReopository extends CrudRepository<Order1, Long> {
 
-//    @Query(value = "CALL insert_to_order_order_item(:shipping_adress,:id_user,:phone,:ngnhan,:productIds,:quantities);", nativeQuery = true)
+    //    @Query(value = "CALL insert_to_order_order_item(:shipping_adress,:id_user,:phone,:ngnhan,:productIds,:quantities);", nativeQuery = true)
 //    int insert_to_order_order_item(@Param("shipping_adress") String shipping_adress,
 //                         @Param("id_user") long id_user,
 //                         @Param("phone") String phone,
@@ -24,13 +25,14 @@ public interface OrderReopository extends CrudRepository<Order1, Long> {
 //                         @Param("quantities") String quantities);
 //    @Procedure(name = "getOrder_byIdUser")
 //    List<OrderDTO> getOrder_byIdUser(@Param("id_user") long id_user);
-@Query(value = "CALL  insert_to_order_order_item(:shipping_adress,:id_user,:phone,:ngnhan,:productIds,:quantities);", nativeQuery = true)
-int insert_to_order_order_item(@Param("shipping_adress") String shipping_adress,
-                         @Param("id_user") long id_user,
-                         @Param("phone") String phone,
-                         @Param("ngnhan") String ngnhan,
-                         @Param("productIds") String productIds,
-                         @Param("quantities") String quantities);
+    @Query(value = "CALL  insert_to_order_order_item(:shipping_adress,:id_user,:phone,:ngnhan,:productIds,:quantities)", nativeQuery = true)
+    int insert_to_order_order_item(@Param("shipping_adress") String shipping_adress,
+                                   @Param("id_user") long id_user,
+                                   @Param("phone") String phone,
+                                   @Param("ngnhan") String ngnhan,
+                                   @Param("productIds") String productIds,
+                                   @Param("quantities") String quantities);
+
     @Query(value = "CALL getOrder_byIdUser_by_date(:id_User,:start,:end)", nativeQuery = true)
     List<OrderDTO> getOrder_byIdUser_by_date(@Param("id_User") long id_User,
                                              @Param("start") String start,
@@ -45,9 +47,10 @@ int insert_to_order_order_item(@Param("shipping_adress") String shipping_adress,
 
     @Query(value = "CALL getOrder_byIdUser_evaluate(:id_User)", nativeQuery = true)
     List<OrderDTO> getOrder_byIdUser_evaluate(@Param("id_User") long id_User);
+
     @Query(value = "CALL get_order_by_date(:start,:end)", nativeQuery = true)
     List<Order1> get_order_by_date(@Param("start") String start,
-                                     @Param("end") String end);
+                                   @Param("end") String end);
 
     @Query(value = "CALL delete_order(:idorder);", nativeQuery = true)
     Long delete_order(@Param("idorder") long idorder);
